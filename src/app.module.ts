528362e4de70f0eeb2b11dd350d2e8cc571a2c4e
Ibrahim Fakharany <ibraheemelfakharany@gmail.com>
2024-01-18 15:50:27 +0200

    initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,45 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
+      // type: 'postgres',
+      // host: 'ws-instance.cdcca2wqwdyv.eu-north-1.rds.amazonaws.com',
+      // port: 5432,
+      // password: '12345678',
+      // username: 'white_solutions',
+      // entities: [User],
+      // database: 'swapy_db',
+      // synchronize: true,
+      // logging: true,
+      // ssl: true,
+      // extra: {
+      //   trustServerCertificate: false,
+      //   Encrypt: true,
+      //   IntegratedSecurity: true,
+      // },
+
       type: 'postgres',
-      host: 'https://swapiy.cdcca2wqwdyv.eu-north-1.rds.amazonaws.com',
+      host: 'ws-instance.cdcca2wqwdyv.eu-north-1.rds.amazonaws.com',
       port: 5432,
+      username: 'white_solutions',
       password: '12345678',
-      username: 'postgres',
-      entities: [User],
-      database: 'swapiy',
-      synchronize: true,
-      logging: true,
+      database: 'swapy_db',
+      autoLoadEntities: true,
+      synchronize: false,
+      ssl: true,
+      extra: {
+        trustServerCertificate: true,
+        Encrypt: true,
+        IntegratedSecurity: false,
+      },
     }),
     UserModule,
   ],
